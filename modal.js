cd7ee2be6ba1bfae8b92eea95ffc97aacd7f0888
Pimsickgirl <75786797+Pimsickgirl@users.noreply.github.com>
2023-11-28 16:14:23 +0700

    Add files via upload

---
+++ modal.js
@@ -0,0 +1,17 @@
+
+function openModal() {
+    document.getElementById('myModal').style.display = 'flex';
+}
+
+function closeModal() {
+    document.getElementById('myModal').style.display = 'none';
+}
+
+window.onclick = function (event) {
+    var modal = document.getElementById('myModal');
+    if (event.target == modal) {
+        closeModal();
+    }
+};
+
+window.onload = openModal;
